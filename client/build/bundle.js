90f8dbd2bacc7d87d43641fbddd62d9124b5b791
Will Robertson <w.robertson305@gmail.com>
2017-02-05 15:15:23 +0000

    gitignore

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -11,16 +11,16 @@
 /******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,143 +33,169 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	var UI = __webpack_require__(1);
-	
-	var app = function() {
-	  var ui = new UI();
-	  
-	}
-	
-	window.onload = app;
+var Logic = __webpack_require__(2);
+var MythFacts = __webpack_require__(3);
 
-/***/ },
+var UI = function () {
+  this.counter = 0;
+  this.logic = new Logic();
+  this.mythFacts = new MythFacts();
+  this.nextCharacter();
+}
+
+UI.prototype = {
+  render: function(mythInfo){
+    var infoDiv = document.querySelector("#info");
+    infoDiv.innerHTML = null; 
+    var mythParagraph = document.createElement("p");
+    mythParagraph.innerText = mythInfo;
+
+    infoDiv.appendChild(mythParagraph);
+
+    if (this.counter !== this.logic.characters.length){
+      this.createButton();
+    }
+  },
+
+  createButton: function(){
+    var button = document.createElement("button");
+    button.innerText = "presss meee";
+    var info = document.querySelector("#info")
+    info.appendChild(button);
+    button.onclick = this.nextCharacter.bind(this);
+    console.log(this);
+
+  },
+
+  nextCharacter: function(){
+    this.mythFacts.getWiki(this.logic.characters[this.counter].name, this.render.bind(this));
+    this.counter ++;
+  }
+}
+
+module.exports = UI;
+
+/***/ }),
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-	var Logic = __webpack_require__(3);
-	var MythFacts = __webpack_require__(2);
-	
-	var UI = function () {
-	  this.counter = 0;
-	  this.logic = new Logic();
-	  this.mythFacts = new MythFacts();
-	  this.nextCharacter();
-	}
-	
-	UI.prototype = {
-	  render: function(mythInfo){
-	    var infoDiv = document.querySelector("#info");
-	    infoDiv.innerHTML = null; 
-	    var mythParagraph = document.createElement("p");
-	    mythParagraph.innerText = mythInfo;
-	
-	    infoDiv.appendChild(mythParagraph);
-	
-	    if (this.counter !== this.logic.characters.length){
-	      this.createButton();
-	    }
-	  },
-	
-	  createButton: function(){
-	    var button = document.createElement("button");
-	    button.innerText = "presss meee";
-	    var info = document.querySelector("#info")
-	    info.appendChild(button);
-	    button.onclick = this.nextCharacter.bind(this);
-	    console.log(this);
-	
-	  },
-	
-	  nextCharacter: function(){
-	    this.mythFacts.getWiki(this.logic.characters[this.counter].name, this.render.bind(this));
-	    this.counter ++;
-	  }
-	}
-	
-	module.exports = UI;
+var Character = function(name){
+  this.name = name;
+}
 
-/***/ },
+Character.prototype = {
+
+}
+
+module.exports = Character;
+
+/***/ }),
 /* 2 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	var MythFacts = function(){
-	
-	}
-	
-	MythFacts.prototype = {
-	  makeRequest: function(url, callback){
-	    var request = new XMLHttpRequest();
-	    request.open("GET", url);
-	    request.onload = callback;
-	    request.send();
-	  },
-	
-	  getWiki: function(mythQuery, callback){
-	    var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + mythQuery + "&origin=*";
-	    this.makeRequest(url, function(){
-	      if (this.status !== 200){
-	        return;
-	      } else {
-	        var info = JSON.parse(this.responseText).query.pages;
-	        var pageKey = Object.keys(info)[0];
-	        var page = info[pageKey].extract;
-	        callback(page);
-	      }
-	    });
-	  }
-	
-	}
-	
-	module.exports = MythFacts;
+var Character = __webpack_require__(1);
 
+var Logic = function() {
+  this.generateCharacter();
+}
 
-/***/ },
+Logic.prototype = {
+  generateCharacter: function(){
+    this.characters = [];
+    this.characters.push(new Character("Heracles"));
+    this.characters.push(new Character("Apollo"));
+    this.characters.push(new Character("Athena"));
+    this.characters.push(new Character("Jason"));
+  }
+}
+
+module.exports = Logic;
+
+/***/ }),
 /* 3 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-	var Character = __webpack_require__(4);
-	
-	var Logic = function() {
-	  this.generateCharacter();
-	}
-	
-	Logic.prototype = {
-	  generateCharacter: function(){
-	    this.characters = [];
-	    this.characters.push(new Character("Heracles"));
-	    this.characters.push(new Character("Apollo"));
-	    this.characters.push(new Character("Athena"));
-	    this.characters.push(new Character("Jason"));
-	  }
-	}
-	
-	module.exports = Logic;
+var MythFacts = function(){
 
-/***/ },
+}
+
+MythFacts.prototype = {
+  makeRequest: function(url, callback){
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = callback;
+    request.send();
+  },
+
+  getWiki: function(mythQuery, callback){
+    var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + mythQuery + "&origin=*";
+    this.makeRequest(url, function(){
+      if (this.status !== 200){
+        return;
+      } else {
+        var info = JSON.parse(this.responseText).query.pages;
+        var pageKey = Object.keys(info)[0];
+        var page = info[pageKey].extract;
+        callback(page);
+      }
+    });
+  }
+
+}
+
+module.exports = MythFacts;
+
+
+/***/ }),
 /* 4 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	var Character = function(name){
-	  this.name = name;
-	}
-	
-	Character.prototype = {
-	
-	}
-	
-	module.exports = Character;
+var UI = __webpack_require__(0);
 
-/***/ }
+var app = function() {
+  var ui = new UI();
+  
+}
+
+window.onload = app;
+
+/***/ })
 /******/ ]);
 //# sourceMappingURL=bundle.js.map
