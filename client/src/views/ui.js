3c0c5bc72e2087a9bff301f965a094b147554032
Will Robertson <w.robertson305@gmail.com>
2017-02-05 16:09:59 +0000

    wiki infor displays in-game

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -2,35 +2,6 @@ var Logic = require('../models/logic');
 var ViewLogic = require('../models/viewLogic')
 var MythFacts = require('../models/mythFacts');
 
-//   render: function(mythInfo){
-//     var infoDiv = document.querySelector("#info");
-//     infoDiv.innerHTML = null; 
-//     var mythParagraph = document.createElement("p");
-//     mythParagraph.innerText = mythInfo;
-
-//     infoDiv.appendChild(mythParagraph);
-
-//     if (this.counter !== this.logic.characters.length){
-//       this.createButton();
-//     }
-//   },
-
-//   createButton: function(){
-//     var button = document.createElement("button");
-//     button.innerText = "presss meee";
-//     var info = document.querySelector("#info")
-//     info.appendChild(button);
-//     button.onclick = this.nextCharacter.bind(this);
-//     console.log(this);
-
-//   },
-
-//   nextCharacter: function(){
-//     this.mythFacts.getWiki(this.logic.characters[this.counter].name, this.render.bind(this));
-//     this.counter ++;
-//   }
-// }
-
 var UI = function (){
   this.counter = 0;
   this.logic = new Logic();
@@ -39,26 +10,61 @@ var UI = function (){
   this.viewLogic = new ViewLogic();
   this.view = document.querySelector('#view');
   this.content = document.querySelector("#content");
-  this.button = document.createElement('button');
+  // this.button = document.createElement('button');
   this.body = document.querySelector('body');
-  this.inputButton = document.createElement("input");
-  this.inputButton.value = 700;
-  this.body.appendChild(this.inputButton);
-  this.body.appendChild(this.button);
-  this.button.innerText = "test movement";
-  this.button.onclick = function(){
-    this.viewLogic.scrollMaster(400, this.exampleInfo);
-  }.bind(this);
+  this.infoDiv = null;
+  this.infoDiv = document.createElement("div");
+  // this.inputButton = document.createElement("input");
+  // this.inputButton.value = 700;
+  // this.body.appendChild(this.inputButton);
+  // this.body.appendChild(this.button);
+  // this.button.innerText = "test movement";
+  // this.button.onclick = 
+  this.createButton();
 }
 
 UI.prototype = {
-  exampleInfo: function(){
-    var divvy = document.createElement('div');
-    divvy.innerHTML = "<p>I am some fascinating facts</p>";
+  // exampleInfo: function(){
+  //   var divvy = document.createElement('div');
+  //   divvy.innerHTML = "<p>I am some fascinating facts</p>";
+  //   var container = document.querySelector("#container");
+  //   container.appendChild(divvy);
+  //   divvy.className = "fact-box";
+  // },
+
+  render: function(mythInfo){
     var container = document.querySelector("#container");
-    container.appendChild(divvy);
-    divvy.className = "fact-box";
+    this.infoDiv = document.createElement("div");
+    this.infoDiv.innerHTML = null; 
+    var mythParagraph = document.createElement("p");
+    mythParagraph.innerText = mythInfo;
+    this.infoDiv.className = "fact-box";
+    this.infoDiv.appendChild(mythParagraph);
+    container.appendChild(this.infoDiv);
+    var buttonDiv = document.querySelector('#button-div')
+    buttonDiv.innerHTML = null;
+    if (this.counter !== this.logic.characters.length){
+      this.createButton();
+    }
   },
+
+  createButton: function(){
+    var buttonDiv = document.querySelector('#button-div')
+    buttonDiv.innerHTML = null;
+    var button = document.createElement("button");
+    button.innerText = "presss meee";
+    // var info = document.querySelector("#info")
+    buttonDiv.appendChild(button);
+    button.onclick = function(){
+      this.infoDiv.innerHTML = null;
+    this.viewLogic.scrollMaster(400, this.nextCharacter.bind(this));
+  }.bind(this);
+  },
+
+  nextCharacter: function(){
+    this.mythFacts.getWiki(this.logic.characters[this.counter].name, this.render.bind(this));
+    this.counter ++;
+  }
 }
 
 module.exports = UI;
